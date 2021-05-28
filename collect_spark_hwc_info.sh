2fe50d588700eceaebc1a801f2051088185142e8
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-28 10:21:00 +0530

    Update collect_spark_hwc_info.sh

--- collect_spark_hwc_info.sh
+++ collect_spark_hwc_info.sh
@@ -4,7 +4,8 @@ echo ""
 echo "Running $0 script"
 echo ""
 
-hive_site_xml_file="/etc/hive/conf/hive-site.xml"
+hive_site_xml_file=$(ls /etc/hive/conf/hive-site.xml)
+beeline_site_xml_file=$(ls /etc/hive/*_tez/beeline-site.xml)
 cdp_directory="/opt/cloudera/parcels/CDH"
 is_cdp_cluster=$([ -d $cdp_directory ] && echo true)
 
@@ -20,27 +21,16 @@ if [ ! -d "$hwc_directory" ]; then
   exit 0
 fi
 
-if [ ! -f "$hive_site_xml_file" ]; then
-    echo "${hive_site_xml_file} does not exist on this host, or the current user $(whoami) does not have access to the files."
+if [ [ ! -f "$hive_site_xml_file"] || [ ! -f "$beeline_site_xml_file" ] ]; then
+    echo "${hive_site_xml_file}/$beeline_site_xml_file does not exist on this host <$(hostname -f)>, or the current user <$(whoami)> does not have access to the files."
+    exit 0
 fi
 
 hive_metastore_uri=$(grep "thrift.*9083" "$hive_site_xml_file" |awk -F"<|>" '{print $3}')
-hive_jdbc_url=$(grep "hive.zookeeper.quorum" -A1 "$hive_site_xml_file" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
+hive_jdbc_url=$(grep "beeline.hs2.jdbc.url.hive" -A1 "$beeline_site_xml_file" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
 hwc_jar=$(find $hwc_directory -name hive-warehouse-connector-assembly-*.jar)
 hwc_pyfile=$(find $hwc_directory -name pyspark_hwc-*.zip)
 
-echo "spark.sql.hive.hiveserver2.jdbc.url=jdbc:hive2://${hive_jdbc_url}"
-echo "spark.datasource.hive.warehouse.metastoreUri=${hive_metastore_uri}"
-echo "spark.datasource.hive.warehouse.load.staging.dir=/tmp"
-echo "spark.jars=${hwc_jar}"
-echo "spark.submit.pyFiles=${hwc_pyfile}"
-echo "spark.datasource.hive.warehouse.read.via.llap=false"
-echo "spark.datasource.hive.warehouse.read.mode=DIRECT_READER_V2"
-echo "spark.sql.hive.hwc.execution.mode=spark"
-echo "spark.datasource.hive.warehouse.read.jdbc.mode=cluster"
-echo "spark.security.credentials.hiveserver2.enabled=false"
-echo "spark.sql.extensions=com.qubole.spark.hiveacid.HiveAcidAutoConvershension"
-echo "spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator"
 echo ""
 echo "spark-shell --master yarn \ "
 echo "  --conf spark.sql.hive.hiveserver2.jdbc.url=jdbc:hive2://${hive_jdbc_url} \ "
@@ -55,3 +45,17 @@ echo "  --conf spark.datasource.hive.warehouse.read.jdbc.mode=cluster \ "
 echo "  --conf spark.security.credentials.hiveserver2.enabled=false \ "
 echo "  --conf spark.sql.extensions=com.qubole.spark.hiveacid.HiveAcidAutoConvershension \ "
 echo "  --conf spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator"
+echo ""
+
+#echo "spark.sql.hive.hiveserver2.jdbc.url=${hive_jdbc_url}"
+#echo "spark.datasource.hive.warehouse.metastoreUri=${hive_metastore_uri}"
+#echo "spark.datasource.hive.warehouse.load.staging.dir=/tmp"
+#echo "spark.jars=${hwc_jar}"
+#echo "spark.submit.pyFiles=${hwc_pyfile}"
+#echo "spark.datasource.hive.warehouse.read.via.llap=false"
+#echo "spark.datasource.hive.warehouse.read.mode=DIRECT_READER_V2"
+#echo "spark.sql.hive.hwc.execution.mode=spark"
+#echo "spark.datasource.hive.warehouse.read.jdbc.mode=cluster"
+#echo "spark.security.credentials.hiveserver2.enabled=false"
+#echo "spark.sql.extensions=com.qubole.spark.hiveacid.HiveAcidAutoConvershension"
+#echo "spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator"
