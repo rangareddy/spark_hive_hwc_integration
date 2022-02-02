47ea2895cfe63d62358186b3c70e5a61e84a6d60
Ranga Reddy <rangareddy.avula@gmail.com>
2022-02-02 11:23:52 +0530

    Update collect_spark_hwc_info.sh

--- collect_spark_hwc_info.sh
+++ collect_spark_hwc_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+#set -x
 
 # log_info() is used to log the message based on logging level. By default logging level will be INFO.
 log_info() {
@@ -17,6 +17,7 @@ log_info() {
     fi
 }
 
+echo ""
 log_info "Running $0 script"
 
 hive_site_xml_file_path="/etc/hive/conf/hive-site.xml"
@@ -35,9 +36,9 @@ script_usage() {
     	ERROR_MSG="<hive-site.xml> file does not exist on this host or the current user <$(whoami)> does not have access to ${hive_site_xml_file_path} file."
     fi
 
-    if [ -z "$ERROR_MSG" ]; then
-    	log_info "ERROR" ERROR_MSG
-    	exit 1
+    if [ ! -z "$ERROR_MSG" ]; then
+        log_info "ERROR" ${ERROR_MSG}
+        exit 1
     fi
 }
 
@@ -45,7 +46,7 @@ script_usage
 
 hive_jdbc_url=""
 if [ -z "$beeline_site_xml_file" ]; then
-    log_info "WARN" "<beeline-site.xml> file does not exist on this host or the current user <$(whoami)> does not have access to the files."
+    #log_info "WARN" "<beeline-site.xml> file does not exist on this host or the current user <$(whoami)> does not have access to the files."
     
     hive_zookeeper_quorum=$(grep "hive.zookeeper.quorum" -A1 "$hive_site_xml_file" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
     hive_zookeeper_port=$(grep "hive.zookeeper.client.port" -A1 "$hive_site_xml_file" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
@@ -71,7 +72,8 @@ hive_metastore_uri=$(grep "thrift.*9083" "$hive_site_xml_file" |awk -F"<|>" '{pr
 hwc_jar=$(find $hwc_directory -name hive-warehouse-connector-assembly-*.jar)
 #hwc_pyfile=$(find $hwc_directory -name pyspark_hwc-*.zip)
 
-echo ""
+echo "Launch the spark-shell by coping following command"
+echo "======================================================"
 echo "spark-shell --master yarn \ "
 echo "  --conf spark.sql.hive.hiveserver2.jdbc.url='${hive_jdbc_url}' \ "
 echo "  --conf spark.datasource.hive.warehouse.metastoreUri='${hive_metastore_uri}' \ "
@@ -85,6 +87,19 @@ echo "  --conf spark.datasource.hive.warehouse.read.jdbc.mode=cluster \ "
 echo "  --conf spark.security.credentials.hiveserver2.enabled=false \ "
 echo "  --conf spark.sql.extensions=com.qubole.spark.hiveacid.HiveAcidAutoConvershension \ "
 echo "  --conf spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator"
+echo "======================================================"
+echo ""
+echo "After launching the spark-shell run the following code"
+echo ""
+echo "import com.hortonworks.hwc.HiveWarehouseSession"
+echo "import com.hortonworks.hwc.HiveWarehouseSession._"
+echo "val hive = HiveWarehouseSession.session(spark).build()"
+echo "val tableName = \"hwc_test_table\""
+echo "hive.createTable(tableName).ifNotExists().column(\"id\", \"bigint\").column(\"name\", \"string\").column(\"age\", \"int\").create()"
+echo "val dataFrame = Seq((1l, \"Ranga\", 34),(2l, \"Nishanth\", 30)).toDF(\"id\", \"name\", \"age\")"
+echo "dataFrame.write.format(\"com.hortonworks.spark.sql.hive.llap.HiveWarehouseConnector\").mode(\"overwrite\").option(\"table\", tableName).save()"
+echo "hive.executeQuery(s\"select * from \${tableName}\").show"
 echo ""
 
-log_info "Run the above spark-shell command to test the HWC in CDP cluster."
+log_info "$0 script finished"
+
