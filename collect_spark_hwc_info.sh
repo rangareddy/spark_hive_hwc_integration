beb32e9f710567f71410a376f2cde4ee43350634
Ranga Reddy <rangareddy.avula@gmail.com>
2022-02-02 16:06:32 +0530

    Update collect_spark_hwc_info.sh

--- collect_spark_hwc_info.sh
+++ collect_spark_hwc_info.sh
@@ -23,6 +23,10 @@ hive_site_xml_file=$(ls ${hive_site_xml_file_path})
 beeline_site_xml_file=$(find /etc -name beeline-site.xml)
 hwc_directory="/opt/cloudera/parcels/CDH/lib/hive_warehouse_connector/"
 
+
+IS_SPARK_SHELL="${IS_SPARK_SHELL:-true}"
+IS_PYSPARK_SHELL="${IS_PYSPARK_SHELL:-false}"
+
 `type klist > /dev/null;` && IS_KERBERIZED=true || IS_KERBERIZED=false
 
 script_usage() {
@@ -73,43 +77,112 @@ fi
 
 hive_metastore_uri=$(grep "thrift.*9083" "$hive_site_xml_file" |awk -F"<|>" '{print $3}')
 hwc_jar=$(find $hwc_directory -name hive-warehouse-connector-assembly-*.jar)
-#hwc_pyfile=$(find $hwc_directory -name pyspark_hwc-*.zip)
+hwc_pyfile=$(find $hwc_directory -name pyspark_hwc-*.zip)
 
-log_info "Launch the spark-shell by coping the following command"
-echo "======================================================"
-echo "spark-shell --master yarn \ "
-echo "  --conf spark.sql.hive.hiveserver2.jdbc.url='${hive_jdbc_url}' \ "
-echo "  --conf spark.datasource.hive.warehouse.metastoreUri='${hive_metastore_uri}' \ "
-echo "  --conf spark.datasource.hive.warehouse.load.staging.dir=/tmp \ "
-echo "  --conf spark.jars=${hwc_jar} \ "
-#echo "  --conf spark.submit.pyFiles=${hwc_pyfile} \ "
-echo "  --conf spark.datasource.hive.warehouse.read.via.llap=false \ "
-echo "  --conf spark.datasource.hive.warehouse.read.mode=DIRECT_READER_V2 \ "
-echo "  --conf spark.sql.hive.hwc.execution.mode=spark \ "
-echo "  --conf spark.datasource.hive.warehouse.read.jdbc.mode=cluster \ "
-echo "  --conf spark.sql.extensions=com.hortonworks.spark.sql.rule.Extensions \ "
-#echo "  --conf spark.sql.extensions=com.qubole.spark.hiveacid.HiveAcidAutoConvershension \ "
-echo "  --conf spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator \ "
-if [ ${IS_KERBERIZED} ]; then
-    echo "  --conf spark.security.credentials.hiveserver2.enabled=true \ "
-    user_prin=$(grep "hive.server2.authentication.kerberos.principal" -A1 "${hive_site_xml_file}" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
-    echo "  --conf spark.sql.hive.hiveserver2.jdbc.url.principal=${user_prin}"
-else
-    echo "  --conf spark.security.credentials.hiveserver2.enabled=false"
+generate_spark_shell_script() {
+	echo ""
+	log_info "Launch the spark-shell by coping the following command"
+	echo "======================================================"
+	echo "spark-shell --master yarn \ "
+	echo "  --conf spark.sql.hive.hiveserver2.jdbc.url='${hive_jdbc_url}' \ "
+	echo "  --conf spark.datasource.hive.warehouse.metastoreUri='${hive_metastore_uri}' \ "
+	echo "  --conf spark.datasource.hive.warehouse.load.staging.dir=/tmp \ "
+	echo "  --conf spark.jars=${hwc_jar} \ "
+	echo "  --conf spark.datasource.hive.warehouse.read.via.llap=false \ "
+	echo "  --conf spark.datasource.hive.warehouse.read.mode=DIRECT_READER_V2 \ "
+	echo "  --conf spark.sql.hive.hwc.execution.mode=spark \ "
+	echo "  --conf spark.datasource.hive.warehouse.read.jdbc.mode=cluster \ "
+	echo "  --conf spark.sql.extensions=com.hortonworks.spark.sql.rule.Extensions \ "
+	#echo "  --conf spark.sql.extensions=com.qubole.spark.hiveacid.HiveAcidAutoConvershension \ "
+	echo "  --conf spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator \ "
+	if [ ${IS_KERBERIZED} ]; then
+	    echo "  --conf spark.security.credentials.hiveserver2.enabled=true \ "
+	    user_prin=$(grep "hive.server2.authentication.kerberos.principal" -A1 "${hive_site_xml_file}" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
+	    echo "  --conf spark.sql.hive.hiveserver2.jdbc.url.principal=${user_prin}"
+	else
+	    echo "  --conf spark.security.credentials.hiveserver2.enabled=false"
+	fi
+	echo "======================================================"
+	echo ""
+
+	echo "After launching the spark-shell run the following code"
+	echo "------------------------------------------------------"
+	echo ""
+	echo "import com.hortonworks.hwc.HiveWarehouseSession"
+	echo "import com.hortonworks.hwc.HiveWarehouseSession._"
+	echo "val hwc = HiveWarehouseSession.session(spark).build()"
+	echo "val tableName = \"hwc_test_table\""
+	echo ""
+	echo "// Creating a Hive table if not exists"
+	echo "hwc.createTable(tableName).ifNotExists().column(\"id\", \"bigint\").column(\"name\", \"string\").column(\"age\", \"int\").create()"
+	echo ""
+	echo "// Saving the data to Hive table"
+	echo "val dataFrame = Seq((1l, \"Ranga\", 34),(2l, \"Nishanth\", 30)).toDF(\"id\", \"name\", \"age\")"
+	echo "dataFrame.write.format(\"com.hortonworks.spark.sql.hive.llap.HiveWarehouseConnector\").mode(\"overwrite\").option(\"table\", tableName).save()"
+	echo ""
+	echo "// Reading the data from Hive table"
+	echo "hwc.executeQuery(s\"select * from \${tableName}\").show"
+	echo ""
+	echo "------------------------------------------------------"
+	echo ""
+}
+
+generate_pyspark_shell_script() {
+	echo ""
+	log_info "Launch the pyspark by coping the following command"
+	echo "======================================================"
+	echo "pyspark --master yarn \ "
+	echo "  --conf spark.sql.hive.hiveserver2.jdbc.url='${hive_jdbc_url}' \ "
+	echo "  --conf spark.datasource.hive.warehouse.metastoreUri='${hive_metastore_uri}' \ "
+	echo "  --conf spark.datasource.hive.warehouse.load.staging.dir=/tmp \ "
+	echo "  --conf spark.jars=${hwc_jar} \ "
+	echo "  --conf spark.submit.pyFiles=${hwc_pyfile} \ "
+	echo "  --conf spark.datasource.hive.warehouse.read.via.llap=false \ "
+	echo "  --conf spark.datasource.hive.warehouse.read.mode=DIRECT_READER_V2 \ "
+	echo "  --conf spark.sql.hive.hwc.execution.mode=spark \ "
+	echo "  --conf spark.datasource.hive.warehouse.read.jdbc.mode=cluster \ "
+	echo "  --conf spark.sql.extensions=com.hortonworks.spark.sql.rule.Extensions \ "
+	#echo "  --conf spark.sql.extensions=com.qubole.spark.hiveacid.HiveAcidAutoConvershension \ "
+	echo "  --conf spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator \ "
+	if [ ${IS_KERBERIZED} ]; then
+	    echo "  --conf spark.security.credentials.hiveserver2.enabled=true \ "
+	    user_prin=$(grep "hive.server2.authentication.kerberos.principal" -A1 "${hive_site_xml_file}" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
+	    echo "  --conf spark.sql.hive.hiveserver2.jdbc.url.principal=${user_prin}"
+	else
+	    echo "  --conf spark.security.credentials.hiveserver2.enabled=false"
+	fi
+	echo "======================================================"
+	echo ""
+
+	echo "After launching the pyspark run the following code"
+	echo "------------------------------------------------------"
+	echo ""
+	echo "from pyspark.sql import SparkSession"
+	echo "from pyspark_llap import HiveWarehouseSession"
+	echo "hwc = HiveWarehouseSession.session(spark).build()"
+	echo "tableName = \"hwc_test_table\""
+	echo ""
+	echo "// Creating a Hive table if not exists"
+	echo "hwc.createTable(tableName).ifNotExists().column(\"id\", \"bigint\").column(\"name\", \"string\").column(\"age\", \"int\").create()"
+	echo ""
+	echo "// Saving the data to Hive table"
+	echo "dataFrame = spark.createDataFrame([(1l, \"Ranga\", 34),(2l, \"Nishanth\", 30)], [\"id\", \"name\", \"age\"])"
+	echo "dataFrame.write.format(\"com.hortonworks.spark.sql.hive.llap.HiveWarehouseConnector\").mode(\"overwrite\").option(\"table\", tableName).save()"
+	echo ""
+	echo "// Reading the data from Hive table"
+	echo "hwc.executeQuery(\"select * from \"+tableName).show()"
+	echo ""
+	echo "------------------------------------------------------"
+	echo ""
+	echo ""
+}
+
+if [ ${IS_SPARK_SHELL} ]; then
+	generate_spark_shell_script
 fi
-echo "======================================================"
-echo ""
 
-echo "After launching the spark-shell run the following code"
-echo ""
-echo "import com.hortonworks.hwc.HiveWarehouseSession"
-echo "import com.hortonworks.hwc.HiveWarehouseSession._"
-echo "val hive = HiveWarehouseSession.session(spark).build()"
-echo "val tableName = \"hwc_test_table\""
-echo "hive.createTable(tableName).ifNotExists().column(\"id\", \"bigint\").column(\"name\", \"string\").column(\"age\", \"int\").create()"
-echo "val dataFrame = Seq((1l, \"Ranga\", 34),(2l, \"Nishanth\", 30)).toDF(\"id\", \"name\", \"age\")"
-echo "dataFrame.write.format(\"com.hortonworks.spark.sql.hive.llap.HiveWarehouseConnector\").mode(\"overwrite\").option(\"table\", tableName).save()"
-echo "hive.executeQuery(s\"select * from \${tableName}\").show"
-echo ""
+if [ ${IS_PYSPARK_SHELL} == true ]; then
+	generate_pyspark_shell_script
+fi
 
 log_info "$0 script finished"
